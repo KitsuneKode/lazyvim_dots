fa44cc82d8cca05c845cf16fa05b64862b549957
Manash Pratim Bhuyan <bhuyanmanash2002@gmail.com>
2026-04-08 17:52:41 +0530

    vscode msg fix

--- lua/plugins/vscode-extend.lua
+++ lua/plugins/vscode-extend.lua
@@ -2,6 +2,17 @@ if not vim.g.vscode then
   return {}
 end
 
+if vim.g.vscode then
+  vim.opt.cmdheight = 1 -- Or higher if messages still pop up
+end
+
+if vim.g.vscode then
+  vim.g.clipboard = vim.g.vscode_clipboard
+  vim.opt.clipboard = "unnamedplus"
+  vim.opt.shortmess:append("sS") -- hide search count and wrap messages like [1/7], W
+  vim.opt.showcmd = false -- hide partial command display like /keybo
+end
+
 local vscode = require("vscode")
 
 vim.api.nvim_create_autocmd("User", {
